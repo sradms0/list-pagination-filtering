d9f353fccc78125e592610c2cb0229cfe4affe87
sradms0 <sradms0@openmailbox.org>
2019-05-18 18:00:48 -0400

    add addPageLinks function and set up init for page-load

--- js/script.js
+++ js/script.js
@@ -13,4 +13,47 @@ const showPage = (list, page) => {
   });
 }
 
+const addPageLinks = list => {
+  const pages = Math.ceil(students.length / perPage);
+  const div = document.createElement('div');
+  const ul = document.createElement('ul');
+
+  div.className = 'pagination';
+  div.appendChild(ul);
+  document.querySelector('.page').appendChild(div);
+
+  // create anchors
+  for (let i = 1; i <= pages; i++) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    // set anchor page #, and href 
+    a.textContent = i;
+    a.href='#';
+
+    // append anchor and li accordingly
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+
+  // add listeners to anchors via propagation
+  ul.addEventListener('click', e => {
+    if (e.target.tagName === 'A') {
+      const a = e.target;
+
+      // swap active class to clicked anchor
+      document.querySelector('.active').className = '';
+      a.className = 'active';
+
+      // show that page
+      showPage(list, a.textContent);
+    }
+  })
+}
+
+// show first page and create links on page-load
 showPage(students, 1);
+addPageLinks(students);
+
+// set first page to active
+document.querySelector('a').className = 'active';
