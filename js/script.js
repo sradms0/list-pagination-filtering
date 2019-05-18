db2b6da85e612fa826e00112cde7c00de151df55
sradms0 <sradms0@openmailbox.org>
2019-05-18 16:57:11 -0400

    add showPage function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 const students = document.querySelectorAll('.student-item');
 const perPage = 10;
+
+const showPage = (list, page) => {
+  const max = page * perPage;
+  const min = max - perPage;
+
+  // display students if in current range, or hide them
+  list.forEach((i, idx) => {
+    if (idx >= min && idx < max) {
+      i.style.display = '';
+    } else i.style.display = 'none';
+  });
+}
+
+showPage(students, 1);
